73c8044868f7e9c6c27a7372a3b2b4f039345972
Diba Mirza <diba@Dibas-MacBook-Air.local>
2026-05-11 09:57:09 -0700

    lect13

---
+++ lect12/graph.cpp
@@ -0,0 +1,42 @@
+// graph.cpp
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+/*
+Unweighted graph
+Edge List E = {  (0, 2), 
+                 (2, 3), (2, 1), (2, 4), 
+                 (3, 1),  
+                 (1, 5), (1, 4),
+                 (4, 5)
+                 }
+
+Write the Adjacency list representation for the example graph
+
+
+Weighted graph
+Edge List E = {  (0, 2, 0.5), 
+                 (2, 3, 0.1), (2, 1, 0.7), (2, 4, 0.2), 
+                 (3, 1, 0.3),  
+                 (1, 5, 0.1), (1, 4, 0.9),
+                 (4, 5, 0.3)
+                 }
+*/
+int main(int argc, char const *argv[])
+{
+    // Graph g(6); 
+    // g.addEdge(0, 2); 
+    // g.addEdge(2, 3);
+    // g.addEdge(2, 1);
+    // g.addEdge(2, 4);
+    // g.addEdge(3, 1);
+    // g.addEdge(1, 5);
+    // g.addEdge(1, 4);
+    // g.addEdge(4, 5);
+
+    // g.display();
+    return 0;
+}
