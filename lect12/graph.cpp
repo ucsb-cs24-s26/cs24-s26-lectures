f2f8ade868367bb14e4d10b188be8176eef81fcb
Diba Mirza <diba@Dibas-MacBook-Air.local>
2026-05-11 20:46:57 -0700

    lect 12: graph representation

--- lect12/graph.cpp
+++ lect12/graph.cpp
@@ -1,9 +1,30 @@
 // graph.cpp
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
-
+class Graph{
+    public:
+        Graph(int n){
+            adjlist.resize(n);
+        }
+        void addEdge(int source, int dest){
+            adjlist[source].insert(dest);  // Directed graph
+        }
+        void display(){
+            for(int u = 0; u < adjlist.size(); u++ ){
+                cout << u << " : ";
+                for (auto v : adjlist[u]){
+                    cout << v << ", ";
+                }
+                cout << endl;
+            }
+        }
+    
+    private:
+        vector<unordered_set<int>> adjlist;
+};
 
 /*
 Unweighted graph
@@ -16,6 +37,14 @@ Edge List E = {  (0, 2),
 
 Write the Adjacency list representation for the example graph
 
+adjlist = { {2},
+            {4, 5},
+            {3, 1, 4},
+            {1},
+            {5},
+            {}
+}
+
 
 Weighted graph
 Edge List E = {  (0, 2, 0.5), 
@@ -27,16 +56,16 @@ Edge List E = {  (0, 2, 0.5),
 */
 int main(int argc, char const *argv[])
 {
-    // Graph g(6); 
-    // g.addEdge(0, 2); 
-    // g.addEdge(2, 3);
-    // g.addEdge(2, 1);
-    // g.addEdge(2, 4);
-    // g.addEdge(3, 1);
-    // g.addEdge(1, 5);
-    // g.addEdge(1, 4);
-    // g.addEdge(4, 5);
+    Graph g(6); 
+    g.addEdge(0, 2); 
+    g.addEdge(2, 3);
+    g.addEdge(2, 1);
+    g.addEdge(2, 4);
+    g.addEdge(3, 1);
+    g.addEdge(1, 5);
+    g.addEdge(1, 4);
+    g.addEdge(4, 5);
 
-    // g.display();
+    g.display();
     return 0;
 }
