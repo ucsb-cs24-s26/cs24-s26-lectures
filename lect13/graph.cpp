e31ba52efa6354b94f34232a14e007880525f9c5
Diba Mirza <diba@169-231-117-58.wireless.ucsb.edu>
2026-05-13 14:50:14 -0700

    lect 13

--- lect13/graph.cpp
+++ lect13/graph.cpp
@@ -12,7 +12,8 @@ class Graph {
             adjlist.resize(n);
         }
         void addEdge(int source, int dest) {
-            adjlist[source].insert(dest);  // Directed graph
+            adjlist[source].insert(dest); 
+            adjlist[dest].insert(source); 
         }
         void display() {
             for (int u = 0; u < adjlist.size(); u++) {
@@ -25,6 +26,23 @@ class Graph {
         }
         void exploreBFS(int source) {
             // TODO: implement BFS traversal starting from source
+            queue<int> q;
+            int n = adjlist.size();
+            vector<bool> visited(n, false);
+            visited[source] = true;
+            cout << source << " ";
+            q.push(source);
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+                for(auto v : adjlist[u]){
+                    if(!visited[v]){
+                        visited[v] = true;
+                        cout << v << " ";
+                        q.push(v);
+                    }
+                }
+            }
         }
 
     private:
@@ -32,6 +50,7 @@ class Graph {
 };
 
 /*
+(Set of vertices) V = {0, 1, 2, 3, 4, 5}
 Edge List E = {  (0, 2),
                  (2, 3), (2, 1), (2, 4),
                  (3, 1),
@@ -52,6 +71,8 @@ int main(int argc, char const *argv[])
     g.addEdge(4, 5);
 
     g.display();
-    // g.exploreBFS(3);
+    cout << "Breadth First Traversal" <<endl;
+    g.exploreBFS(0);
+    cout << endl;
     return 0;
 }
